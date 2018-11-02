b9b967dcfd579e53141b1110603dd546a74db629
suki60 <fran.altes@gmail.com>
2018-11-02 17:57:40 +0100

    first commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'slim'
+
+group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+  gem 'pry'
+end
---
+++ francesc-altes.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+
+if development?
+  require "sinatra/reloader"
+  require 'pry'
+  require "better_errors"
+end
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = __dir__
+end
+
+get '/' do
+  slim :home
+end
